4c6e3adad14466d176711b28a02ffb019ee88744
vasu <vasutomar26@gmail.com>
2025-12-06 14:04:49 +0530

    Reconfigured pipeline

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use("/api/workout", workoutRoutes);
 app.use("/api/exercises", exercisesRoutes);
 
 module.exports = app;
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 app.listen(port, () =>
         console.log(`Server running on port ${port}`)
       );
