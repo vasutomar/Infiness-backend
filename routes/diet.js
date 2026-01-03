ef84f8335af8fa3c0bcf981f5f70972386c2717a
vasu <vasutomar26@gmail.com>
2026-01-04 02:51:11 +0530

    Integrated with openai

--- routes/diet.js
+++ routes/diet.js
@@ -7,6 +7,12 @@ const Diet = require("../models/Diet");
 
 dotenv.config();
 
+const OpenAI = require("openai");
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+
 router.get("/health", function (req, res) {
   res.json({
     status: "Running",
@@ -48,13 +54,31 @@ router.put("/", async (req, res) => {
       goal,
     } = req.body;
     const userId = req.user.id;
+
+    // Generating OpenAPI response
+    let openAPI_input = `Generate a weekly diet plan for me.`
+    if (height) openAPI_input+=` I am ${height} cms tall.`;
+    if (weight) openAPI_input+=` I weigh ${weight} kgs.`;
+    if (goal) openAPI_input+=`  My goals are ${goal.toString()}.`;
+    if (diet) openAPI_input+=` I follow ${diet.toString()} diet.`;
+    if (cuisine) openAPI_input+=`  My cusinie is ${cuisine.toString()}`;
+    if (restriction) openAPI_input+=`  I have following restrictions: ${restriction}.`;
+    if (preference) openAPI_input+=` I prefer: ${preference}.`; 
+    openAPI_input+= `Generate in a json format of type { day: "Monday", breakfast: "....", lunch: "....", dinner: "....", calorieCount: "700KCals", proteinCount: "20g" }`;
+    openAPI_input+= `. only return JSON and no extra text.`;
+
+    const chatGPTResponse = await client.responses.create({
+      model: "gpt-4o",
+      input: openAPI_input,
+    });
+
     const response = await Diet.findOneAndUpdate(
       {
         userId,
       },
       {
         userId,
-        plan: [],
+        plan: JSON.parse(chatGPTResponse.output_text),
         age,
         height,
         preference,
