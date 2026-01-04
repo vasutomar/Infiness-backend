80dba25e27339f560a420789b72423762bd84da8
vasu <vasutomar26@gmail.com>
2026-01-04 14:50:16 +0530

    Used schema for structuring openai responses

--- models/Diet.js
+++ models/Diet.js
@@ -1,13 +1,24 @@
 const mongoose = require("mongoose");
 
+const MealSchema = new mongoose.Schema({
+  calories: { type: Number, required: true },
+  protein: { type: Number, required: true },
+  meal: { type: String, required: true }
+})
+
 const PlanSchema = new mongoose.Schema({
   day: { type: String, required: true },
-  breakfast: { type: String, required: true },
-  lunch: { type: String, required: true },
-  dinner: { type: String, required: true },
-  calorieCount: { type: String, required: true },
-  proteinCount: { type: String, required: true },
+  breakfast: { type: MealSchema, required: true },
+  lunch: { type: MealSchema, required: true },
+  dinner: { type: MealSchema, required: true },
+  totalCalories: { type: String, required: true },
+  proteinIntake: { type: String, required: true },
 });
+
+const PlanWithTipsSchema = new mongoose.Schema({
+  week: { type: [PlanSchema], required: false },
+  tips: { type: [String], required: false }
+})
 
 const DietSchema = new mongoose.Schema({
   userId: {
@@ -15,7 +26,7 @@ const DietSchema = new mongoose.Schema({
     ref: "users",
     required: true,
   },
-  plan: { type: [PlanSchema], required: true },
+  plan: { type: PlanWithTipsSchema, required: true },
   age: { type: String },
   height: { type: String },
   preference: { type: String },
@@ -25,6 +36,7 @@ const DietSchema = new mongoose.Schema({
   diet: { type: [String] },
   cuisine: { type: [String] },
   goal: { type: [String] },
+  planCreated: {type: Boolean, default: true }
 });
 
 module.exports = mongoose.model("Diet", DietSchema);
--- routes/diet.js
+++ routes/diet.js
@@ -8,9 +8,69 @@ const Diet = require("../models/Diet");
 dotenv.config();
 
 const OpenAI = require("openai");
+const { format } = require("winston");
 const client = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
+const dietPlanSchema = {
+  type: "object",
+  additionalProperties: false,
+  properties: {
+    week: {
+      type: "array",
+      minItems: 7,
+      maxItems: 7,
+      items: {
+        type: "object",
+        additionalProperties: false,
+        properties: {
+          day: { type: "string" },
+          breakfast: {
+            type: "object",
+            additionalProperties: false,
+            properties: {
+              meal: { type: "string" },
+              calories: { type: "number" },
+              protein: {type: "number"}
+            },
+            required: ["meal", "calories", "protein"]
+          },
+          lunch: {
+            type: "object",
+            additionalProperties: false,
+            properties: {
+              meal: { type: "string" },
+              calories: { type: "number" },
+              protein: {type: "number"}
+            },
+            required: ["meal", "calories", "protein"]
+          },
+          dinner: {
+            type: "object",
+            additionalProperties: false,
+            properties: {
+              meal: { type: "string" },
+              calories: { type: "number" },
+              protein: {type: "number"}
+            },
+            required: ["meal", "calories", "protein"]
+          },
+          totalCalories: { type: "number" },
+          proteinIntake: { type: "number" }
+        },
+        required: ["day", "breakfast", "lunch", "dinner", "totalCalories", "proteinIntake"]
+      }
+    },
+    tips: {
+      type: "array",
+      minItems: 3,
+      maxItems: 7,
+      items: { type: "string" }
+    }
+  },
+  required: ["week", "tips"]
+};
 
 
 router.get("/health", function (req, res) {
@@ -31,9 +91,18 @@ router.get("/questions", async (req, res) => {
 router.get("/", async (req, res) => {
   try {
     const userId = req.user.id;
-    const response = await Diet.find({
+    const response = await Diet.findOne({
       userId,
     });
+    if (!response) {
+      res.json(null);
+      return;
+    } else if (response.planCreated == false) {
+      res.json({
+        planCreated: false,
+        msg: "Plan creation is in progress",
+      });
+    }
     res.json(response);
   } catch (err) {
     res.status(500).send("Server error", err.message);
@@ -56,21 +125,46 @@ router.put("/", async (req, res) => {
     const userId = req.user.id;
 
     // Generating OpenAPI response
-    let openAPI_input = `Generate a weekly diet plan for me.`
-    if (height) openAPI_input+=` I am ${height} cms tall.`;
-    if (weight) openAPI_input+=` I weigh ${weight} kgs.`;
-    if (goal) openAPI_input+=`  My goals are ${goal.toString()}.`;
-    if (diet) openAPI_input+=` I follow ${diet.toString()} diet.`;
-    if (cuisine) openAPI_input+=`  My cusinie is ${cuisine.toString()}`;
-    if (restriction) openAPI_input+=`  I have following restrictions: ${restriction}.`;
-    if (preference) openAPI_input+=` I prefer: ${preference}.`; 
-    openAPI_input+= `Generate in a json format of type { day: "Monday", breakfast: "....", lunch: "....", dinner: "....", calorieCount: "700KCals", proteinCount: "20g" }`;
-    openAPI_input+= `. only return JSON and no extra text.`;
+    let openAPI_input = `Generate a weekly diet plan for me.`;
+    if (height) openAPI_input += ` I am ${height} cms tall.`;
+    if (weight) openAPI_input += ` I weigh ${weight} kgs.`;
+    if (goal) openAPI_input += `  My goals are ${goal.toString()}.`;
+    if (diet) openAPI_input += ` I follow ${diet.toString()} diet.`;
+    if (cuisine) openAPI_input += `  My cusinie is ${cuisine.toString()}`;
+    if (restriction)
+      openAPI_input += `  I have following restrictions: ${restriction}.`;
+    if (preference) openAPI_input += ` I prefer: ${preference}.`;
+    openAPI_input += `. only return JSON and no extra text.`;
 
-    const chatGPTResponse = await client.responses.create({
-      model: "gpt-4o",
-      input: openAPI_input,
-    });
+    client.responses
+      .create({
+        model: "gpt-5.1",
+        input: openAPI_input,
+        text: {
+          format: {
+            type: "json_schema",
+            name: "plan",
+            schema: dietPlanSchema,
+            strict: true
+          },
+        },
+      })
+      .then(async (res) => {
+        try {
+          let parsedResponse = JSON.parse(res.output_text);
+          await Diet.findOneAndUpdate(
+            {
+              userId,
+            },
+            {
+              plan: parsedResponse,
+              planCreated: true,
+            }
+          );
+        } catch (err) {
+          console.log(err);
+        }
+      });
 
     const response = await Diet.findOneAndUpdate(
       {
@@ -78,7 +172,7 @@ router.put("/", async (req, res) => {
       },
       {
         userId,
-        plan: JSON.parse(chatGPTResponse.output_text),
+        plan: [],
         age,
         height,
         preference,
@@ -88,6 +182,7 @@ router.put("/", async (req, res) => {
         diet,
         cuisine,
         goal,
+        planCreated: false,
       },
       { upsert: true, returnDocument: "after" }
     );
